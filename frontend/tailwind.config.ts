70922e065e387059b142406a6f073454d134585a
Denis <denis.jullien@seabubbles.com>
2024-11-26 11:59:12 +0100

    test front page layout

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -2,12 +2,30 @@ import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
 import type { Config } from 'tailwindcss';
 
+// 1. Import the Skeleton plugin
+import { join } from 'path';
+import { skeleton } from '@skeletonlabs/tw-plugin';
+
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	// 2. Opt for dark mode to be handled via the class method
+	darkMode: 'class',
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		// 3. Append the path to the Skeleton package
+		join(require.resolve(
+				'@skeletonlabs/skeleton'),
+			'../**/*.{html,js,svelte,ts}'
+		)
+	],
 
 	theme: {
 		extend: {}
 	},
 
-	plugins: [typography, forms]
+	plugins: [
+		typography, forms,
+		skeleton({
+			themes: { preset: [ "skeleton" ] }
+		})
+	]
 } satisfies Config;
