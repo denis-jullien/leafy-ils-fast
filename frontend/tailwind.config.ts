aad4d55c1d6d2fe9eae4ac4289132def074de510
Denis <denis.jullien@seabubbles.com>
2024-11-27 18:33:21 +0100

    add daisy ui & saraui

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -2,20 +2,12 @@ import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
 import type { Config } from 'tailwindcss';
 
-// 1. Import the Skeleton plugin
-import { join } from 'path';
-import { skeleton } from '@skeletonlabs/tw-plugin';
-
 export default {
-	// 2. Opt for dark mode to be handled via the class method
 	darkMode: 'class',
 	content: [
 		'./src/**/*.{html,js,svelte,ts}',
-		// 3. Append the path to the Skeleton package
-		join(require.resolve(
-				'@skeletonlabs/skeleton'),
-			'../**/*.{html,js,svelte,ts}'
-		)
+		//👇 add Sara UI
+		"./node_modules/saraui/**/*.{html,js,svelte,ts}",
 	],
 
 	theme: {
@@ -24,8 +16,9 @@ export default {
 
 	plugins: [
 		typography, forms,
-		skeleton({
-			themes: { preset: [ "skeleton" ] }
-		})
-	]
+		require("daisyui"),
+	],
+	daisyui: {
+		themes: ["emerald"],
+	},
 } satisfies Config;
