30e701257af9a01e59184a430821cddc570030f3
Denis <denis.jullien@seabubbles.com>
2024-12-01 03:11:56 +0100

    remove unnecessary code

--- backend/unit_tests/conftest.py
+++ backend/unit_tests/conftest.py
@@ -1,8 +1,6 @@
 # conftest.py
 # see https://gist.github.com/peterhurford/09f7dcda0ab04b95c026c60fa49c2a68 to introdue modularity
 import pytest
-import sys
-from pathlib import Path
 
 from fastapi.testclient import TestClient
 from sqlmodel import Session, SQLModel, create_engine
@@ -10,15 +8,6 @@ from sqlmodel.pool import StaticPool
 
 from backend.database import get_session
 from backend.main import app
-
-# Add the src directory to sys.path for all test files
-project_root = Path(__file__).resolve().parents[1]  # Navigate to the root directory
-sys.path.append(str(project_root))
-sys.path.append(str(project_root / "backend"))
-
-
-
-
 
 @pytest.fixture(name="session")
 def session_fixture():
